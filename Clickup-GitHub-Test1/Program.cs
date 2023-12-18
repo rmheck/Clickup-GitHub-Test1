7e6ac7713bae49757888f3caf9caac302904eab0
Robert Heck <rmheck@sabreindustries.com>
2023-12-18 15:32:03 -0600

    Add project files.

---
+++ Clickup-GitHub-Test1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Test1-Main Line 1");
+Console.WriteLine("Test1-Main Line 2");
+Console.WriteLine("Test1-Main Line 3");
